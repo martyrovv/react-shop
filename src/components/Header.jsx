0357ee637f24d85872e16d24d054bfb20c790182
Марат Мартин <maratmartin@MacBook-Air-Marat.local>
2025-01-27 16:54:31 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav>
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Rep</a></li>
+                <li><a href="https://github.com/martyrovv/react-shop" target="_blank" rel="noreferrer">Rep</a></li>
             </ul>
         </div>
     </nav>
